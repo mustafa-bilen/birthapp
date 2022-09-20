e00b7490b3e58403dd14e11d73567bc31096f8d6
mustafa-bilen <mustafabilen6669@gmail.com>
2022-09-20 19:47:58 +0300

    enterkey added

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const calculate = document.getElementById("button");
 const final = document.querySelector(".input");
 calculate.addEventListener("click", () => {
   const age = new Date().getFullYear() - year.value;
-  console.log(age);
-  // input.classList.add("hidden");
-  // button.classList.add("hidden");
-  // document.querySelector(".input").innerHTML = `you are  ${age} years old`;
   final.innerHTML = `you are  ${age} years old`;
+});
+
+year.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    calculate.click();
+  }
 });
